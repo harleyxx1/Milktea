89ef3d6264f46195ea81534552667ca491ba9f1f
Harleeeey <dingharley123@gmail.com>
2020-12-15 13:39:18 +0800

    #commit make the header responsive.

--- src/react/header/src/Header.js
+++ src/react/header/src/Header.js
@@ -1,26 +1,92 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState} from 'react'
 import { Styles } from './styles';
 
-class Header extends Component {
-    render() {
-        return (
-            <div style={Styles.headerContainer}>
-                <div style={Styles.headerLogoNameContainer}>
-                    <p className="logoName" style={Styles.headerLogoName}>Milkteahan</p>
-                </div>
-                <div style={{flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                    <a className="menuButtons" href="" style={Styles.menuButtons}>About us</a>
-                    <a className="menuButtons" href="" style={Styles.menuButtons}>Locations</a>
-                    <a className="menuButtons" href="" style={Styles.menuButtons}>Menu</a>
-                    <a className="menuButtons" href="" style={Styles.menuButtons}>Contact Us</a>
-                    <a className="menuButtons" href="" style={Styles.menuButtons}>Faq</a>
-                </div>
-                <div style={{flex: .7}}>
+import './style.css';
 
-                </div>
-            </div>
-        )
+export default function Header() {
+
+    /**
+     * Usestates.
+     */
+    const [ collapseNav, setCollapseNav] = useState(false);
+    const [ headerChanges, setHeaderChanges ] = useState({
+        headerContainer: 'headerContainer',
+        headerLogoContainer: 'headerLogoContainer',
+        loginButtonContainer: 'loginButtonContainer',
+    });
+
+    const [ windowSize, setWindowSize ] = useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+    })
+
+    /**
+     * UseEffects.
+     */
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+    useEffect(() => {
+        if(collapseNav){
+            setHeaderChanges({
+                headerContainer: headerChanges.headerContainer + ' headerContainerScrolled',
+                headerLogoContainer: headerChanges.headerLogoContainer + ' headerLogoContainerScrolled',
+                loginButtonContainer: headerChanges.loginButtonContainer + ' loginButtonContainerScrolled'
+            })
+        }else{
+            setHeaderChanges({
+                headerContainer: 'headerContainer',
+                headerLogoContainer: 'headerLogoContainer',
+                loginButtonContainer: 'loginButtonContainer'
+            })
+        }        
+    }, [collapseNav])
+
+    /**
+     * The callback for the scroll event.
+     * 
+     * @param {Object} e - The result of resize event.
+     */
+    const handleResize = (e) => {
+        setWindowSize({
+            height: window.innerHeight,
+            width: window.innerWidth
+        })
     }
+    
+    /**
+     * The callback for the scroll event.
+     * 
+     * @param {Object} e - The result of scroll event.
+     */
+    const handleScroll = (e) => {
+        if(window.scrollY > 50){
+            setCollapseNav(true) 
+            return;
+        }
+        
+        setCollapseNav(false);
+        return;
+    }
+    
+    return (
+        <div className={headerChanges.headerContainer} style={Styles.headerContainer}>
+            <div className={headerChanges.headerLogoContainer} style={{...Styles.headerLogoNameContainer(windowSize.width), ... windowSize.width < 1200 && {justifyContent: 'start'}}}>
+                <p className="logoName" style={Styles.headerLogoName(windowSize.width)}>Milkteahan</p>
+            </div>
+            <div className="menuButtonsContainer" style={{...Styles.menuButtonsContainer, ... windowSize.width < 1200 && {display: 'none'}}}>
+                <a className="menuButtons" href="" style={Styles.menuButtons}>About</a>
+                <a className="menuButtons" href="" style={Styles.menuButtons}>Locations</a>
+                <a className="menuButtons" href="" style={Styles.menuButtons}>Menu</a>
+                <a className="menuButtons" href="" style={Styles.menuButtons}>Contact</a>
+                <a className="menuButtons" href="" style={Styles.menuButtons}>Faq</a>
+            </div>
+            <div className={headerChanges.loginButtonContainer} style={{...Styles.loginButtonContainer}}>
+                <a className="loginButton" href="" style={Styles.loginButtonStyle(windowSize.width)}>Log in</a>
+            </div>
+        </div>
+    )
 }
 
-export default Header
--- src/react/header/src/styles.js
+++ src/react/header/src/styles.js
@@ -1,31 +1,58 @@
 export const Styles = {
     headerContainer: {
-        height: '20vh', 
         display: 'flex', 
-        position: 'absolute', 
+        position: 'fixed', 
         top: 0, 
         right: 0, 
         left: 0, 
-        flexDirection: 
-        'row'
+        flexDirection: 'row',
     },
 
-    headerLogoName: {
-        color: 'white', 
-        fontSize: '40px'
+    headerLogoName: (width) => {
+        return {
+            color: 'white', 
+            fontSize: width < 800 ? `30px` : `${width * .03}px`,
+            marginRight: '50px'
+        }
     },
 
-    headerLogoNameContainer: {
-        flex: .7, 
-        marginLeft: '40px',
-        marginRight: '20px',
+    headerLogoNameContainer: (width) => {
+        return {
+            flex: .7, 
+            marginLeft: `${width <= 400 ? '40px' : '0px'}`,
+            display: 'flex', 
+            alignItems: 'center', 
+        }
+    },
+
+    loginButtonContainer: {
+        alignItems: 'center',
         display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'flex-end'
+        flex: .7, 
+    },
+
+    loginButtonStyle: (width) => {
+        return{ 
+            alignItems: 'center',
+            backgroundColor: 'white',
+            color: 'black',
+            display: 'flex',
+            height: '40px',
+            justifyContent: 'center',
+            marginLeft: `${width > 500? '80': '0'}px`,
+            width: '100px',
+        }
     },
     
     menuButtons: {
         color: 'white',
         padding: '30px',
+    },
+
+    menuButtonsContainer: {
+        flex: 1,
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'center'
     }
 }
